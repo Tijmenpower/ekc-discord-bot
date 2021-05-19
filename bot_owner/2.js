1a52090529a5c8569aafed8f3a6aaa1148c90b6b
ItsNotMy <50448591+Tijmenpower@users.noreply.github.com>
2021-05-19 22:34:05 +0200

    Update 2.js

--- bot_owner/2.js
+++ bot_owner/2.js
@@ -2,11 +2,13 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 exports.run = async (client, message, args) => {
-  if (message.author.id == "630029784302485524") {
+  if (message.author.id == "362983908532420608","362981513777905665") {
     var gametoset = args.join(' ');
     if (!gametoset) gametoset = null;
     client.user.setActivity(gametoset);
     message.reply("The new game has been set!");
+    message.react('✅');
+    setTimeout(() => message.delete(), 10000)
     } else {
       message.reply("sorry alleen mijn maker kan dit doen. :x:");
     }
